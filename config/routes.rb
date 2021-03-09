7973d1a707c5d7ecbfff4ffa4899bbfee58385ba
vmwhoami <vmwhoami@gmail.com>
2021-03-09 15:52:21 +0200

    Upgrade ruby and rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources  :books
-  # get 'books/index'
-
-  # get 'books/destroy' to "books#delete"
-  # get 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_111748) do
+ActiveRecord::Schema.define(version: 2021_03_09_134857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_111748) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
